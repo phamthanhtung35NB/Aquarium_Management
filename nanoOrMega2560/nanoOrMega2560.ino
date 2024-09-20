13edaaa963f9a3c63e9729da9471f0aec472dcc3
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-09-20 21:49:50 +0700

    set

--- nanoOrMega2560/nanoOrMega2560.ino
+++ nanoOrMega2560/nanoOrMega2560.ino
@@ -1,33 +1,24 @@
-/*
-            JoyStick                Arduino
-              VCC                     5V
-              GND                    GND
-              VRx                     A0
-              VRy                     A1
-              SW                      2
-
-   Nạp code mở Serial Monitor chọn No line ending, baud 9600.
-
-*/
-
 const int bientroX = A0 ;
 const int bientroY = A1 ;
-const int button = 2;
+const int btn_bom = 9;
 const int ena = 3;
 const int in1 = 4;
-const int in2 = 5;
-int tocdo=4;
-int xf;
-int yf;
+const int in2_bom = 8;
+const int role_led = 5;
+bool is_bom = true;
+bool is_led = true;
+bool is_nhay = true;
+
+int tocdo = 3;
 
 //chân ST_CP của 74HC595
-int latchPin = 8;
+int latchPin = 10;
 //chân SH_CP của 74HC595
 int clockPin = 12;
 //Chân DS của 74HC595
 int dataPin = 11;
-const int capBac[10] ={0,1,2,3,4,5,6,7,8,9};
-const int tocDo[10] ={0,167,178,189,200,211,222,233,244,255};
+const int capBac[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+const int tocDo[10] = {0, 167, 178, 189, 200, 211, 222, 233, 244, 255};
 // Mảng cho LED 7 đoạn chung cực âm
 // Các bit 1 sẽ làm các đoạn sáng
 const int Seg[20] = {
@@ -55,28 +46,99 @@ const int Seg[20] = {
 
 void setup ()
 {
-  pinMode(2, INPUT);
+  Serial.begin(9600);
+
+  //  xf = analogRead(bientroX);
+  //  yf = analogRead(bientroY);
+
+  //brown switch
+  pinMode(btn_bom, INPUT);
   pinMode(bientroX, INPUT);
   pinMode(bientroY, INPUT);
-  pinMode (ena, OUTPUT);
-  pinMode (in1, OUTPUT);
-  pinMode (in2, OUTPUT);
-  xf = analogRead(bientroX);
 
-  yf = analogRead(bientroY);
-
-  Serial.begin(9600);
-    //Bạn BUỘC PHẢI pinMode các chân này là OUTPUT
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
+
+  pinMode(role_led, OUTPUT);
+  digitalWrite (role_led, 1);
+
+  pinMode (ena, OUTPUT);
+  pinMode (in1, OUTPUT);
+  analogWrite (ena, tocDo[tocdo]);
+  digitalWrite (in1, HIGH);
+
+  pinMode(in2_bom, OUTPUT);
+  digitalWrite (in2_bom, HIGH);
+
+  delay(1000);
+  is_bom = true;
+  khoiDongLed();
 }
-void motor1_dung()
+void khoiDongLed() {
+  for (int i = 0; i <= 9; i++) {
+    DK_Led(i);
+    delay(150);
+  }
+  DK_Led(9);
+  delay(200);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  DK_Led(9);
+  delay(200);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  DK_Led(9);
+  delay(200);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  for (int i = 0; i <= 9; i++) {
+    DK_Led(i);
+    delay(150);
+  }
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  DK_Led(3);
+  delay(250);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  DK_Led(3);
+  delay(250);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+  DK_Led(3);
+  delay(250);
+  // Tắt tất cả các đoạn của LED
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+  digitalWrite(latchPin, HIGH);
+  delay(200);
+}
+void suc_khi_stop()
 {
+  analogWrite (ena, 0);
   digitalWrite (in1, LOW);
-  digitalWrite (in2, LOW);
 }
-void tangtoc (int i)
+
+void tangToc (int i)
 {
   tocdo += i;
   if (tocdo > 9) {
@@ -85,147 +147,90 @@ void tangtoc (int i)
   analogWrite (ena, tocDo[tocdo]);
   Serial.print("tocDo[tocdo]"); Serial.println(tocDo[tocdo]);
   digitalWrite (in1, HIGH);
-  digitalWrite (in2, LOW);
-  //   delay (50);
 }
 
-void giamtoc(int i)
+void giamToc(int i)
 {
   tocdo -= i;
   if (tocdo < 0) {
     tocdo = 0;
-   motor1_dung();
+    is_nhay = true;
+    suc_khi_stop();
   }
   analogWrite (ena, tocDo[tocdo]);
   Serial.print("tocDo[tocdo]"); Serial.println(tocDo[tocdo]);
   digitalWrite (in1, HIGH);
-  digitalWrite (in2, LOW);
-  //   delay (0);
-
 }
-void controller(int x) {
+void DK_bom_khi(int x) {
   //giam
-  if (xf > 520) {
-    if (xf < 650) {
-      giamtoc(1);
-    } else if (xf < 1200) {
-      giamtoc(2);
+  if (x > 520) {
+    if (x < 650) {
+      giamToc(1);
+    } else if (x < 1200) {
+      giamToc(2);
     }
   }
-  else if (xf < 500) {
-    if (xf < 100) {
-      tangtoc(2);
+  else if (x < 500) {
+    if (x < 100) {
+      tangToc(2);
     } else {
-      tangtoc(1);
+      tangToc(1);
     }
   }
-  Serial.println("-----------------");
-  Serial.print (tocdo);
-  Serial.print (" = ");
-  Serial.println (tocDo[tocdo]);
-  controllerLed(tocdo);
+  DK_Led(tocdo);
   delay(1000);
 }
-void controllerLed(int i){
-  digitalWrite(latchPin, LOW);
-  //Xuất bảng ký tự ra cho Module LED
-  shiftOut(dataPin, clockPin, MSBFIRST, Seg[i]);  
-  
-  digitalWrite(latchPin, HIGH);
+void nhayVaTatLed(int i) {
+  is_nhay = false;
+  delay(500);
+  for (int k = 0; k < 5; k++) {
+    digitalWrite(latchPin, LOW);
+    //Xuất bảng ký tự ra cho Module LED
+    shiftOut(dataPin, clockPin, MSBFIRST, Seg[i]);
+    digitalWrite(latchPin, HIGH);
+    delay(200);
+    // Tắt tất cả các đoạn của LED
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, 0b00000000);
+    digitalWrite(latchPin, HIGH);
+    delay(200);
+  }
 }
-  void loop ()
-  {
-    xf = analogRead(bientroX);  // doc gia tri cua truc X
-    yf = analogRead(bientroY);  // doc gia tri cua truc Y
-//    Serial.print("X="); Serial.println(xf);
-    controller(xf);
-    int KEY = digitalRead(button);  // doc gia tri cua nut nhan
-
-//    Serial.print("X="); Serial.print(xf); Serial.print(" "); Serial.println(xl);
-    //  Serial.print("Y=");Serial.print(yf); Serial.println(yl);
-      Serial.print("KEY="); Serial.print(KEY); Serial.println();
-
-  
-  //  delay(200);//delay để ổn định hơn
-
+void DK_Led(int i) {
+  if (i == 0 && is_nhay == true) {
+    nhayVaTatLed(i);
+  } else if (i > 0) {
+    digitalWrite(latchPin, LOW);
+    //Xuất bảng ký tự ra cho Module LED
+    shiftOut(dataPin, clockPin, MSBFIRST, Seg[i]);
+    digitalWrite(latchPin, HIGH);
+  }
 }
-//int ena = 2;
-//int in1 = 3;
-//int in2 = 4;
-//int in3 = 5;
-//int in4 = 6;
-//int enb = 8;
-//
-//int i;
-//
-//void setup()
-//{
-//  Serial. begin (9600);
-//
-//  pinMode (ena, OUTPUT);
-//  pinMode (in1, OUTPUT);
-//  pinMode (in2, OUTPUT);
-//  pinMode (in3, OUTPUT);
-//  pinMode (in4, OUTPUT);
-//  pinMode (enb, OUTPUT);
-//
-//}
-//
-//void motor1_dung()
-//{
-//  digitalWrite (in1, LOW);
-//  digitalWrite (in2, LOW);
-//}
-//void motor2_dung ()
-//{
-//  digitalWrite (in3, LOW);
-//  digitalWrite (in4, LOW);
-//}
-//
-//void tangtoc ()
-//{
-//
-//  for (i=150; i<256; i+=1)
-//  {
-//     analogWrite (ena, i);
-//     digitalWrite (in1, HIGH);
-//     digitalWrite (in2, LOW);
-////     analogWrite (enb, i);
-//     digitalWrite (in3, LOW);
-//     digitalWrite (in4, HIGH);
-//     Serial.  println (i);
-//     delay (50);
-//  }
-//}
-//
-//void giamtoc ()
-//{
-//
-//  for (i=255; i>150; i-=1)
-//  {
-//     analogWrite (ena, i);
-//     digitalWrite (in1, HIGH);
-//     digitalWrite (in2, LOW);
-////     analogWrite (enb, i);
-//     digitalWrite (in3, LOW);
-//     digitalWrite (in4, HIGH);
-//     Serial.  println (i);
-//     delay (50);
-//  }
-//}
-//
-//void loop()
-//{
-//  tangtoc ();
-//  Serial. println ("Chạy 5 giây");
-//  delay (5000);
-//
-//  giamtoc ();
-//  digitalWrite (in3, LOW);
-//  digitalWrite (in4, LOW);
-//  digitalWrite (in2, LOW);
-//  digitalWrite (in1, LOW);
-//  Serial. println ("Dừng 5 giây");
-//  delay (5000);
-//
-//}
+void loop ()
+{
+  DK_bom_khi(analogRead(bientroX));
+  //Serial.print("=");Serial.print(digitalRead(btn_bom));Serial.print("=");Serial.println(digitalRead(button_led));
+
+  if (analogRead(bientroY) > 1000) {
+    if (is_led == true) {
+      is_led = false;
+      digitalWrite(role_led, 0);
+    } else {
+      is_led = true;
+      digitalWrite(role_led, 1);
+    }
+    delay(500);
+    Serial.println(is_led);
+  }
+  if (digitalRead(btn_bom) == HIGH) {
+    if (is_bom == true) {
+      is_bom = false;
+      digitalWrite(in2_bom, LOW);
+    } else {
+      is_bom = true;
+      digitalWrite(in2_bom, 1);
+    }
+    delay(500);
+    Serial.println(is_bom);
+  }
+}
